3f489dcafc2d2b9a6236cade046e8d9fb857b5ba
nozakifamima <saginogi@gmail.com>
2020-09-15 22:26:42 +0900

    td_align_deleted set_word_update

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,12 @@ class FavoritesController < ApplicationController
   before_action :set_word
 
   def create
-    word = Word.find(params[:word_id])
-    @favorite = current_user.favorites.new(word_id: word.id)
+    @favorite = current_user.favorites.new(word_id: @word.id)
     @favorite.save
   end
 
   def destroy
-    word = Word.find(params[:word_id])
-    @favorite = current_user.favorites.find_by(word_id: word.id)
+    @favorite = current_user.favorites.find_by(word_id: @word.id)
     @favorite.destroy
   end
 
